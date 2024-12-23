a9ca6ee4ea09e5425678cb1d63e362c270596950
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2024-12-23 13:35:05 +0100

    index.php concept_update#1

--- index.php
+++ index.php
@@ -8,15 +8,36 @@ include_once "includes/css_js.inc.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WEBSITE HOMEPAGE</title>
+    <title>Miller's World</title>
     <link rel="stylesheet" href="./dist/<?= $cssPath ?>" />
     <script type="module" src="./dist/<?= $jsPath ?>"></script>
 </head>
 
 <body>
-    <?= "php works on front website" ?>
-    <p class="icon-pacman"></p>
-    <img src="images/sample.jpg" alt="">
+    <header>
+        <nav>
+            <div class="search">
+                <input type="text" name="search" id="search" placeholder="Search for a planet...">
+                <button type="submit">Search</button>
+            </div>
+            <span>Logo</span>
+            <div>
+                <ul class="nav_links">
+                    <li><a href="#">Add a planet</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
+    </header>
+    <main>
+        <?= "php works on the main" ?>
+    </main>
+    <footer>
+        <h3>This is the footer</h3>
+    </footer>
 </body>
 
 </html>

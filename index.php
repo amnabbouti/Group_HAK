a77931c0258a6af7747d22bae52f141b422ec81f
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2024-12-26 13:34:21 +0100

    index.php added pagination concept_update#9

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 include_once "includes/css_js.inc.php";
+require("functions.php");
 
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
-
-// print "<pre>";
-// var_dump($planet_images);
-// print "</pre>";
 
 $accessKey = 'vGiBVlx1ebXeCPSQrofSg68UGu9A_BH2hDitylo3gUU';
 $endpoint = 'https://api.unsplash.com/search/photos';
@@ -23,15 +20,22 @@ if (isset($data['results']) && count($data['results']) > 0) {
     foreach ($data['results'] as $photo) {
         $imageUrls[] = $photo['urls']['regular'];
     }
-    echo '<pre>';
-    print_r($imageUrls);
-    echo '</pre>';
 } else {
     echo "No planets found.";
 }
 
 
+$itemsPerPage = 6;
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$totalItems = count($imageUrls);
+$totalPages = ceil($totalItems / $itemsPerPage);
+$startIndex = ($page - 1) * $itemsPerPage;
+$paginatedItems = array_slice($imageUrls, $startIndex, $itemsPerPage);
+$previousPage = ($page > 1) ? $page - 1 : null;
+$nextPage = ($page < $totalPages) ? $page + 1 : null;
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -58,34 +62,22 @@ if (isset($data['results']) && count($data['results']) > 0) {
                     <li><a href="#">Login</a></li>
                 </ul>
             </nav>
-            <!-- <a href="#"><i class="icon-hak_logo_concept1"></i></a> -->
         </div>
     </header>
+
     <main>
         <section class="main_banner">
             <div class="container">
-                <!-- TODO -> Picture of the month links to the detail.php of that picture -->
                 <a href="#">
-                    <!-- <img src="public/image_moon1.jpg" alt="Image of a Moon"> -->
                     <h3>Picture of the month</h3>
                     <p>Name - Description/Article of planet</p>
                 </a>
             </div>
         </section>
-        <section class="socials">
-            <div class="container">
-                <ul>
-                    <li><a href="#"><i class="icon-pacman"></i></a></li>
-                    <li><a href="#"><i class="icon-video-camera"></i></a></li>
-                    <li><a href="#"><i class="icon-stack"></i></a></li>
-                    <li><a href="#"><i class="icon-connection"></i></a></li>
-                </ul>
-            </div>
-        </section>
+
         <section class="planets">
             <div class="container">
-
-                <?php foreach ($imageUrls as $photo): ?>
+                <?php foreach ($paginatedItems as $photo): ?>
                     <article>
                         <div class="head">
                             <div>
@@ -100,10 +92,25 @@ if (isset($data['results']) && count($data['results']) > 0) {
                         </div>
                     </article>
                 <?php endforeach; ?>
-
+            </div>
+        </section>
+        <section class="pagination">
+            <div class="container">
+                <ul>
+                    <?php if ($previousPage): ?>
+                        <li><a href="?page=<?= $previousPage ?>">Previous</a></li>
+                    <?php endif; ?>
+                    <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                        <li><a href="?page=<?= $i ?>" class="<?= ($i == $page) ? 'active' : '' ?>"><?= $i ?></a></li>
+                    <?php endfor; ?>
+                    <?php if ($nextPage): ?>
+                        <li><a href="?page=<?= $nextPage ?>">Next</a></li>
+                    <?php endif; ?>
+                </ul>
             </div>
         </section>
     </main>
+
     <footer>
         <div class="container">
             <h3>Logo HAK</h3>

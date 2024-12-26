2da20f761e456966ad652f4f0e709b1659c54e80
Hannah Casier <hannah.casier@me.com>
2024-12-26 17:03:21 +0100

    added connection credentials

---
+++ includes/db.inc.php
@@ -0,0 +1,19 @@
+<?php
+function connectToDB()
+{
+    // CONNECTIE CREDENTIALS
+    $db_host = '127.0.0.1';
+    $db_user = 'root';
+    $db_password = 'root';
+    $db_db = 'hak';
+    $db_port = 8889;
+
+    try {
+        $db = new PDO('mysql:host=' . $db_host . '; port=' . $db_port . '; dbname=' . $db_db, $db_user, $db_password);
+    } catch (PDOException $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        die();
+    }
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+    return $db;
+}

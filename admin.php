6e9a16249e00f8fe7a0f6f3e6e41049285f3f73e
Hannah Casier <hannah.casier@me.com>
2025-01-03 15:04:33 +0100

    added html

---
+++ admin.php
@@ -0,0 +1,44 @@
+<?php
+require('db.inc.php');
+require('functions.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+
+<body>
+    <section class="planets">
+        <h1>Planets overview</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>#ID</th>
+                    <th>Planet</th>
+                    <th>Description</th>
+                    <th>Date added</th>
+                    <th>Date edited</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Planet</td>
+                    <td>Description</td>
+                    <td>Date added</td>
+                    <td>Date edited</td>
+                    <td>Actions</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+</body>
+
+</html>

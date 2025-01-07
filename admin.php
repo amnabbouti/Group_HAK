4d4de13315bb6bb8c3edb7c22e01fda9f0843b00
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-01-07 07:28:45 +0100

    update crud admin #4

--- admin.php
+++ admin.php
@@ -1,7 +1,14 @@
 <?php
-require('includes/db.inc.php');
-require('functions.php');
+// Display errors for development
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
 
+// Include necessary files and libraries
+require "includes/db.inc.php";
+include_once "includes/css_js.inc.php";
+require 'functions.php';
+require 'vendor/autoload.php';
 $planets = getPlanets();
 
 // print "<pre>";
@@ -17,44 +24,81 @@ $planets = getPlanets();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin page</title>
 </head>
 
 <body>
-    <section class="planets">
-        <h1>Planets overview</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>#ID</th>
-                    <th>Planet</th>
-                    <th>Description</th>
-                    <th>Date added</th>
-                    <th>Date edited</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($planets as $planet): ?>
+    <header>
+        <nav>
+            <div class="search">
+                <!-- Planet Search -->
+                <form method="get" action="">
+                    <input type="text" name="name" placeholder="Search for a planet..."
+                        value="<?= $_GET['name'] ?? '' ?>">
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+            <a href="#" class="logo">
+                <img src="public/assets/images/logo.svg" alt="Miller's World Logo">
+            </a>
+            <div>
+                <ul class="nav_links">
+                    <li><a href="#">Add a planet</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="planets">
+            <h1>Planets overview</h1>
+            <table>
+                <thead>
                     <tr>
-                        <td><?= $planet['id']; ?></td>
-                        <td><?= $planet['name']; ?></td>
-                        <td><?= mb_strimwidth($planet['description'], 0, 100, "..."); ?></td>
-                        <td><img src="<?= $planet['image']; ?>" alt="<?= $planet['name']; ?>" width="100" height="100"></td>
-                        <td>Not yet in db</td>
-                        <td>Not yet in db</td>
-                        <td>
-                            <a href="detail.php?id=<?= $planet['id']; ?>">View</a>
-                            <a href="edit.php?id=<?= $planet['id']; ?>">Edit</a>
-                            <a href="delete.php?id=<?= $planet['id']; ?>">Delete</a>
-                        </td>
-
+                        <th>#ID</th>
+                        <th>Planet</th>
+                        <th>Description</th>
+                        <th>Date added</th>
+                        <th>Date edited</th>
+                        <th>Actions</th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </section>
+                </thead>
+                <tbody>
+                    <?php foreach ($planets as $planet): ?>
+                        <tr>
+                            <td><?= $planet['id']; ?></td>
+                            <td><?= $planet['name']; ?></td>
+                            <td><?= mb_strimwidth($planet['description'], 0, 100, "..."); ?></td>
+                            <td><img src="<?= $planet['image']; ?>" alt="<?= $planet['name']; ?>" width="100" height="100"></td>
+                            <td>Not yet in db</td>
+                            <td>Not yet in db</td>
+                            <td>
+                                <a href="detail.php?id=<?= $planet['id']; ?>">View</a>
+                                <a href="edit.php?id=<?= $planet['id']; ?>">Edit</a>
+                                <a href="delete.php?id=<?= $planet['id']; ?>">Delete</a>
+                            </td>
 
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </section>
+    </main>
+    <footer>
+        <div class="container">
+
+            <div class="logo">
+                <img src="public/assets/images/logo.svg" alt="Miller's World Logo">
+            </div>
+            <?= "php works on the main & footer" ?>
+            <ul>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 
 </html>

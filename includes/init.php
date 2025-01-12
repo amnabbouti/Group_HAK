3ca0b483087d9a5788bbe0887174a2a2e7af730d
amnabbouti <amine.abbouti@hotmail.com>
2025-01-12 12:09:33 +0100

    Add likes functionality and improved styling.

--- includes/init.php
+++ includes/init.php
@@ -4,6 +4,8 @@ require_once "includes/css_js.inc.php";
 require_once "includes/db.inc.php";
 require_once 'functions.inc.php';
 require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $db = connectToDB();
 if (basename($_SERVER['PHP_SELF']) === 'admin.php' && !empty($_SESSION['refresh_page'])) {
     echo "<script>location.reload();</script>";
--- index.php
+++ index.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 
 include_once 'includes/init.php';
 
-
 $user = getLoggedInUser(); // logged in users
 $nasaData = getNasaFeaturedData();  // nasa api
 
@@ -14,7 +13,7 @@ list($filters, $params) = buildFiltersAndParams($_GET);
 $orderBy = getOrderBy($_GET);
 // Build
 $whereClause = !empty($filters) ? "WHERE " . implode(" AND ", $filters) : "";
-$query = "SELECT id, name, description, image FROM planets $whereClause $orderBy";
+$query = "SELECT id, name, description, image, likes FROM planets $whereClause $orderBy";
 $countQuery = "SELECT COUNT(*) FROM planets $whereClause"; //count
 $stmt = $db->prepare($query);  // planet data (query execution)
 foreach ($params as $key => $value) {
@@ -41,6 +40,7 @@ $pagination = paginate($planetData, $itemsPerPage, $page); // paginated data
 $paginatedItems = $pagination['items'];
 $previousPage = $pagination['previousPage'];
 $nextPage = $pagination['nextPage'];
+
 ?>
 
 <html lang="en">
@@ -71,82 +71,96 @@ $nextPage = $pagination['nextPage'];
                     <button>Explore The Universe</button>
                 </a>
             </div>
-            <!--            <section class="socials">-->
-            <!--                <div class="curiosity-model">-->
-            <!--                    <p id="flight">Discover Space With Miller's World</p>-->
-            <!--                    <model-viewer-->
-            <!--                          id="curiosity"-->
-            <!--                          src="public/assets/models/space_shuttle.glb"-->
-            <!--                          alt="Curiosity Rover"-->
-            <!--                          shadow-intensity="1"-->
-            <!--                          background-color="#000000"-->
-            <!--                          camera-orbit="-75deg auto 1m"-->
-            <!--                          min-camera-orbit="auto auto 20m"-->
-            <!--                          max-camera-orbit="auto auto 20m"-->
-            <!--                          exposure="1"-->
-            <!--                          ground-plane-->
-            <!--                          style="width: 300px; height: 200px; overflow: hidden"-->
-            <!--                          shadow-intensity="1"-->
-            <!--                          environment-image="neutral"-->
-            <!--                          scale="0.5 0.5 0.5"-->
-            <!--                          field-of-view="90deg">-->
-            <!--                    </model-viewer>-->
-            <!--                </div>-->
-            <!--            </section>-->
         </section>
         <section class="filters">
             <form method="GET" action="">
-                <select name="moons" id="moons">
-                    <option value="">Moon Count</option>
-                    <option
-                          value="No Moons" <?= isset($_GET['moons']) && $_GET['moons'] == 'No Moons' ? 'selected' : '' ?>>
-                        No Moons
-                    </option>
-                    <option value="1 Moon" <?= isset($_GET['moons']) && $_GET['moons'] == '1 Moon' ? 'selected' : '' ?>>
-                        1 Moon
-                    </option>
-                    <option
-                          value="More than 1 Moon" <?= isset($_GET['moons']) && $_GET['moons'] == 'More than 1 Moon' ? 'selected' : '' ?>>
-                        More than 1 Moon
-                    </option>
-                </select>
-                <button type="submit">Filter</button>
+                <div class="filter-dropdown">
+                    <a class="filter-dropdown-button" id="dropdownButton">Filters <span class="dropdown-arrow"><i
+                                  class="fa fa-chevron-down"></i></span></a>
+                    <div class="filter-dropdown-content" id="dropdownContent">
+                        <div class="filter-group">
+                            <span class="filter-label">Moon Count</span>
+                            <select name="moons" id="moons">
+                                <option value="">Select Moon Count</option>
+                                <option
+                                      value="No Moons" <?= isset($_GET['moons']) && $_GET['moons'] == 'No Moons' ? 'selected' : '' ?>>
+                                    No Moons
+                                </option>
+                                <option
+                                      value="1 Moon" <?= isset($_GET['moons']) && $_GET['moons'] == '1 Moon' ? 'selected' : '' ?>>
+                                    1 Moon
+                                </option>
+                                <option
+                                      value="More than 1 Moon" <?= isset($_GET['moons']) && $_GET['moons'] == 'More than 1 Moon' ? 'selected' : '' ?>>
+                                    More than 1 Moon
+                                </option>
+                            </select>
+                        </div>
+                        <button type="submit">Apply</button>
+                    </div>
+                </div>
             </form>
-
+            <form method="GET" action="">
+                <div class="filter-dropdown">
+                    <a class="filter-dropdown-button" id="dropdownButton">Sorting <span class="dropdown-arrow"><i
+                                  class="fa fa-chevron-down"></i></span></a>
+                    <div class="filter-dropdown-content" id="dropdownContent">
+                        <div class="filter-group">
+                            <span class="filter-label">A</span>
+                            <select name="filter" id="filter">
+                                <option value="">B</option>
+                            </select>
+                        </div>
+                        <button type="submit">Apply</button>
+                    </div>
+                </div>
+            </form>
         </section>
 
         <section class="planets">
             <?php foreach ($paginatedItems as $planet): ?>
-                <article class="container" id="planets">
-                    <div class="head">
-                        <div>
-                            <a href="detail.php?id=<?= $planet['id']; ?>">
-                                <img src="<?= $planet['image'] ?>"
-                                     alt="<?= $planet['name'] ?>">
-                            </a>
+                <div class="container">
+                    <article id="planets">
+                        <div class="head">
+                            <div>
+                                <a href="detail.php?id=<?= $planet['id']; ?>">
+                                    <img src="<?= $planet['image'] ?>"
+                                         alt="<?= $planet['name'] ?>">
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="foot">
-                        <h3><?= $planet['name'] ?></h3>
-                        <p><?= implode(' ', array_slice(explode(' ', $planet['description']), 0, 10)) . '...'; ?></p>
-                        <a href="detail.php?id=<?= $planet['id']; ?>">DETAILS</a>
-                    </div>
-                </article>
+                        <div class="foot">
+                            <h3><?= $planet['name'] ?></h3>
+                            <p><?= implode(' ', array_slice(explode(' ', $planet['description']), 0, 10)) . '...'; ?></p>
+                            <div class="actions">
+                                <a href="detail.php?id=<?= $planet['id']; ?>">DETAILS</a>
+                                <div class="like-container">
+                                    <span class="like-count"
+                                          id="like-count-<?= $planet['id']; ?>"><?= $planet['likes'] ?? 0 ?></span>
+                                    <button class="like-button" data-planet-id="<?= $planet['id']; ?>">
+                                        <i class="fa-regular fa-heart"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
             <?php endforeach; ?>
         </section>
-
         <!-- Pagination -->
         <section class="pagination">
             <div class="container">
                 <ul>
-                    <?php if ($previousPage): ?>
-                        <li><a href="?page=<?= $previousPage ?>">Previous</a></li>
+                    <?php if ($page > 1): ?>
+                        <li><a href="?page=1"><i class="fa-solid fa-angles-left"></i></a></li>
+                        <li><a href="?page=<?= $previousPage ?>"><i class="fa-solid fa-chevron-left"></i></a></li>
                     <?php endif; ?>
                     <?php for ($i = 1; $i <= $totalPages; $i++): ?>
                         <li><a href="?page=<?= $i ?>" class="<?= ($i == $page) ? 'active' : '' ?>"><?= $i ?></a></li>
                     <?php endfor; ?>
-                    <?php if ($nextPage): ?>
-                        <li><a href="?page=<?= $nextPage ?>">Next</a></li>
+                    <?php if ($page < $totalPages): ?>
+                        <li><a href="?page=<?= $nextPage ?>"><i class="fa-solid fa-chevron-right"></i></a></li>
+                        <li><a href="?page=<?= $totalPages ?>"><i class="fa-solid fa-angles-right"></i></a></li>
                     <?php endif; ?>
                 </ul>
             </div>
